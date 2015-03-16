1226a7a9b5b25eb25054db78e12a9eca6badc6d2
watsond <dylan.watson@thalesgroup.com.au>
2015-03-16 11:22:39 +0800

    Add in-memory cache to server calls - to improve performance and prevent hammering JIRA

--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,52 @@ var path = require('path');
 var fs = require('fs');
 var Client = require('node-rest-client').Client;
 var moment = require('moment');
+var cache = require('memory-cache');
+
+var MINUTE_IN_MILLIS = 60 * 1000;
+var HOUR_IN_MILLIS = 60 * MINUTE_IN_MILLIS;
+var DAY_IN_MILLIS = 24 * HOUR_IN_MILLIS;
 
 var restClient = new Client();
 
 // Extracted from .env
 var username = process.env.JIRA_USERNAME;
 var password = process.env.JIRA_PASSWORD;
+var auth = username ? 'Basic ' + new Buffer(username + ':' + password).toString('base64') : undefined;
 
-var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+function jiraPostRequest(res, jiraUrl, data, cacheTimeout) {
+  var cacheKey = data.jql;
+
+  var args = {
+    path: {},
+    parameters: {},
+    headers: { "Content-Type": "application/json" },
+    requestConfig: {
+      timeout: 10000
+    },
+    data: data,
+  };
+
+  if(auth) {
+    args.headers.Authorization = auth;
+  }
+
+
+  var cachedData = cache.get(cacheKey);
+  if(cachedData) {
+    console.log('Using cache for ' + cacheKey);
+    res.status(304).json(cachedData);
+    return;
+  }
+
+  console.log('Making call to ' + jiraUrl);
+  restClient.post(jiraUrl, args, function (result, response) {
+    cache.put(cacheKey, result, cacheTimeout);
+    res.json(result);
+  });
+}
 
 exports.throughputData = function (req, res) {
-
-  console.log(req.query);
 
   var hostname = req.query.jiraHostName;
   var projects = req.query.projects;
@@ -23,35 +57,15 @@ exports.throughputData = function (req, res) {
 
   var query = "project in (" + projects.join(',') + ") AND issuetype in (" + issueTypes.join(',') + ") AND resolution in (" + completionTypes.join(',') + ") AND resolutiondate > endOfWeek(-23)";
 
-  console.log('requesting ' + query);
-
-  var args = {
-    path: {},
-    parameters: {},
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization" : auth
-    },
-    requestConfig: {
-      timeout: 10000
-    },
-    data: {
-      "jql": query,
-      "maxResults": 250
-    }
+  var data = {
+    "jql": query,
+    "maxResults": 250
   };
 
-  console.log('making throughputData call to ' + hostname + '/rest/api/latest/search');
-
-  restClient.post(hostname + '/rest/api/latest/search', args, function (data, response) {
-    res.json(data);
-  });
-
+  jiraPostRequest(res, hostname + '/rest/api/latest/search', data, DAY_IN_MILLIS);
 };
 
 exports.currentSprint = function (req, res) {
-
-  console.log(req.query);
 
   var hostname = req.query.jiraHostName;
   var projects = req.query.projects;
@@ -59,30 +73,12 @@ exports.currentSprint = function (req, res) {
 
   var query = "project in (" + projects.join(',') + ") AND issuetype in (" + issueTypes.join(',') + ") AND Sprint in openSprints()";
 
-  console.log('requesting ' + query);
-
-  var args = {
-    path: {},
-    parameters: {},
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization" : auth
-    },
-    requestConfig: {
-      timeout: 10000
-    },
-    data: {
-      "jql": query,
-      "maxResults": 250
-    }
+  var data = {
+    "jql": query,
+    "maxResults": 250
   };
 
-  console.log('making currentSprint call to ' + hostname + '/rest/api/latest/search');
-
-  restClient.post(hostname + '/rest/api/latest/search', args, function (data, response) {
-    res.json(data);
-  });
-
+  jiraPostRequest(res, hostname + '/rest/api/latest/search', data, HOUR_IN_MILLIS);
 };
 
 
@@ -95,90 +91,54 @@ exports.allIssuesPerWeek = function (req, res) {
 
   var query = 'project in (' + projects.join(",") + ') AND issuetype in (' + issueTypes.join(",") + ') AND status was in (Open) on endOfWeek(-' + weekNumber + ')';
 
-  var args = {
-    path: {},
-    parameters: {},
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization" : auth
-    },
-    requestConfig: {
-      timeout: 10000
-    },
-    data: {
-      "jql": query,
-      "fields": ["key", "issuetype", "subtasks"],
-      "maxResults": 1500
-    }
+  var data = {
+    "jql": query,
+    "fields": ["key", "issuetype", "subtasks"],
+    "maxResults": 1500
   };
 
-  restClient.post(hostname + '/rest/api/latest/search', args, function (data, response) {
-    res.json(data);
-  });
-
+  jiraPostRequest(res, hostname + '/rest/api/latest/search', data, DAY_IN_MILLIS);
 };
 
 exports.search = function (req, res) {
-
-  console.log(req.query);
 
   var hostname = req.query.jiraHostName;
   var projects = req.query.projects;
   var issueTypes = req.query.issueTypes;
   var search = req.query.search;
 
-  var query = "project in (" + projects.join(',') + ") AND issuetype in (" + issueTypes.join(',') + ") AND (" + search + ")";
+  var query = 'project in (' + projects.join(",") + ') AND issuetype in (' + issueTypes.join(",") + ') AND (' + search + ')';
 
-  console.log('requesting ' + query);
-
-  var args = {
-    path: {},
-    parameters: {},
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization" : auth
-    },
-    requestConfig: {
-      timeout: 10000
-    },
-    data: {
-      "jql": query,
-      "maxResults": 250
-    }
+  var data = {
+    "jql": query,
+    "maxResults": 250
   };
 
-  console.log('making search call to ' + hostname + '/rest/api/latest/search');
-
-  restClient.post(hostname + '/rest/api/latest/search', args, function (data, response) {
-    res.json(data);
-  });
-
+  jiraPostRequest(res, hostname + '/rest/api/latest/search', data, HOUR_IN_MILLIS);
 };
 
 exports.issueDetail = function (req, res) {
 
-  var issueUrl = req.params.issueUrl;
+  var issueUrl = req.query.issueUrl || req.params["issueUrl"];
 
-  var args = {
-    headers: {
-      "Authorization" : auth
-    }
-  };
-
-  restClient.get(issueUrl, args, function (data, response) {
+  console.log(issueUrl);
+  restClient.get(issueUrl, function (data, response) {
     res.json(data);
+  }, function(err) {
+    console.log(err);
   });
-
 };
 
 exports.xml = function (req, res) {
   var url = req.query.url;
 
   var args = {
-    headers: {
-      "Authorization" : auth
-    }
+    headers: {}
   };
+
+  if(auth) {
+    args.headers.Authorization = auth;
+  }
 
   restClient.get(url, args, function (data, response) {
     res.set('Content-Type', 'text/xml');
